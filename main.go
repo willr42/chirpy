2d5053efab9b32c41aca9df945a92b12da6b17cf
Will Robertson <48553811+willr42@users.noreply.github.com>
2026-03-10 21:37:06 +1100

    feat: chapter 2

--- main.go
+++ main.go
@@ -1,14 +1,30 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
 func main() {
+	cfg := apiConfig{fileserverHits: atomic.Int32{}}
 	mux := http.NewServeMux()
-	mux.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
-	mux.HandleFunc("/healthz", handleHealthz)
+	mux.Handle("/app/", cfg.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
+	mux.HandleFunc("GET /metrics", cfg.handleMetrics)
+	mux.HandleFunc("POST /reset", cfg.handleReset)
+	mux.HandleFunc("GET /healthz", handleHealthz)
 
 	server := http.Server{
 		Addr:    ":8080",
@@ -19,7 +35,21 @@ func main() {
 }
 
 func handleHealthz(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(200)
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	w.Write([]byte("OK"))
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK\n"))
+}
+
+func (cfg *apiConfig) handleMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	hits := cfg.fileserverHits.Load()
+	fmt.Fprintf(w, "Hits: %d\n", hits)
+}
+
+func (cfg *apiConfig) handleReset(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	cfg.fileserverHits.Store(0)
+	fmt.Fprintf(w, "Hits reset\n")
 }
