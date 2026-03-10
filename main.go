db82566250289433f20ec604a717ac1106041815
Will Robertson <48553811+willr42@users.noreply.github.com>
2026-03-10 20:35:58 +1100

    feat: serve static file

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
 
-	server.ListenAndServe()
+	log.Fatal(server.ListenAndServe())
 }
